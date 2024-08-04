6bad2ef3c61ad330eeb71f726963611ab6935a03
Minseok Lee <minseok30086@gmail.com>
2024-08-05 00:04:08 +0900

    add frontend and new postgresql database

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
-api.interceptors.request.user(
+api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
     if (token) {
