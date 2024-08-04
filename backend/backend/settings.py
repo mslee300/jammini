4ad4572af133802fe268ed6b5dbdbb28e51b2a4a
Minseok Lee <minseok30086@gmail.com>
2024-08-05 00:40:15 +0900

    Change db back to sqlite

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,12 +97,8 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PWD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
