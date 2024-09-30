cb0774d0017a1930c1a579a00871486823d7af27
Minseok Lee <mslee300@bu.edu>
2024-09-30 15:10:43 -0400

    test frontend

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -4,6 +4,7 @@ import "../styles/Note.css";
 function Note({ note, onDelete }) {
   const formattedDate = new Date(note.created_at).toLocaleDateString("en-US");
 
+  
   return (
     <div className="note-container">
       <p className="note-title">{note.title}</p>
