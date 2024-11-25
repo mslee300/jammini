f70aef3e0a93a6c762e9237a62cc873abd9ab30a
Minseok Lee <mslee300@bu.edu>
2024-11-25 13:55:37 -0500

    configure vite pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,64 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa'
+
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ['favicon.ico', "apple-touc-icon.png", "masked-icon.png" ],
+  manifest: {
+    name: "Weather app",
+    short_name: "Weather app",
+    description: "An app that can show the weather forecast for your city.",
+    icons: [
+      
+      {
+        src: "./icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "./icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose:'favicon'
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes:'180x180',
+        type:'image/png',
+        purpose:'apple touch icon',
+      },
+      {
+        src: "./icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "./icon-256x256.png",
+        sizes: "256x256",
+        type: "image/png",
+        purpose: "icon"
+      },
+      {
+        src: "./icon-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+        purpose: "any maskable"
+      }
+    ],
+    theme_color: "#181818",
+    background_color: "#e8eac2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
   server: {
     historyApiFallback: true,  // This ensures Vite handles client-side routing
     port: 5173,  // Your frontend is running on port 5173
