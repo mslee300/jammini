d26b1835cf283ee8cbbd12587f4fc726dad95ec3
Minseok Lee <mslee300@bu.edu>
2024-12-02 05:59:05 -0500

    try vitepwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,65 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ['favicon.ico', "apple-touch-icon.png", "masked-icon.png" ],
+  manifest: {
+    name: "Jammini",
+    short_name: "Jammini",
+    description: "Turning English learning into a sport.",
+    icons: [
+      
+      {
+        src: "./icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "./icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose:'favicon'
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes:'180x180',
+        type:'image/png',
+        purpose:'apple touch icon',
+      },
+      {
+        src: "./icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "./icon-256x256.png",
+        sizes: "256x256",
+        type: "image/png",
+        purpose: "icon"
+      },
+      {
+        src: "./icon-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+        purpose: "any maskable"
+      }
+    ],
+    theme_color: "#181818",
+    background_color: "#e8eac2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
