c3a0535be131c4b021a4c096bbb5e2760bb73153
Eugene Jung <eugenejung04@gmail.com>
2024-12-02 21:54:53 -0500

    add text logo on starting page

--- frontend/src/pages/LoginOption.jsx
+++ frontend/src/pages/LoginOption.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/LoginOption.css";
 import CharacterLogo from "../assets/img/character-logo.png";
+import TextLogo from "../assets/img/text-logo.png";
 
 const LoginOption = () => {
   const navigate = useNavigate();
@@ -16,11 +17,12 @@ const LoginOption = () => {
 
   return (
     <div className="login-option">
+      <div className="text-logo-container">
+        <img src={TextLogo} alt="Text Logo" className="text-logo" />
+      </div>
       <div className="header">
         <img src={CharacterLogo} alt="Jammini logo" className="logo" />
-        <h2 className="title">
-          게임으로 쉽게 준비하는 토익, 점수높이기.
-        </h2>
+        <h2 className="title">게임으로 쉽게 준비하는 토익, 점수높이기.</h2>
       </div>
       <div className="button-container">
         <button className="signup-button" onClick={handleSignUpClick}>
